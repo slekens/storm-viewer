e176c7cbbadd8927815ac423ebdd0ee24f9a4f0d
Abraham Abreu <slek_2001@hotmail.com>
2022-03-16 11:31:39 -0600

    First commit

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Abraham Abreu on 16/03/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
